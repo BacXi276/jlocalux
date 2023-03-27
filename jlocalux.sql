919f0dd3615d7c1ad96550797cc714252ce8700f
Lucas <98802913+hunykorn@users.noreply.github.com>
2023-03-27 10:47:06 +0200

    Update jlocalux.sql

--- jlocalux.sql
+++ jlocalux.sql
@@ -18,10 +18,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `jlocalux`
+-- Base de données : `localux_db`
 --
 CREATE DATABASE IF NOT EXISTS `jlocalux` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `jlocalux`;
+USE `localux_db`;
 
 -- --------------------------------------------------------
 
